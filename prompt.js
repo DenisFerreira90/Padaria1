afcffcfd01c293bfc7c40396ff829659e80a3418
Denis da Rosa Ferreira <115656630+DenisFerreira90@users.noreply.github.com>
2023-09-06 11:57:58 -0300

    button instal

--- prompt.js
+++ prompt.js
@@ -1,32 +1,17 @@
-let deferredPrompt;
-
 window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevenir que o prompt padrão seja exibido automaticamente
-  e.preventDefault();
-
-  // Armazenar o evento para usá-lo posteriormente
-  deferredPrompt = e;
-
-  // Exibir o botão de instalação ou outro elemento de sua escolha
-  document.getElementById('installButton').style.display = 'block';
-});
-
-document.getElementById('installButton').addEventListener('click', () => {
-  // Verificar se há um evento de instalação adiado
-  if (deferredPrompt) {
-    // Mostrar o prompt de instalação
-    deferredPrompt.prompt();
-
+    // Prevenir que o prompt padrão seja exibido automaticamente
+    e.preventDefault();
+  
+    // Exibir o prompt de instalação automaticamente
+    e.prompt();
+  
     // Aguarde a resposta do usuário
-    deferredPrompt.userChoice.then((choiceResult) => {
+    e.userChoice.then((choiceResult) => {
       if (choiceResult.outcome === 'accepted') {
         console.log('Usuário aceitou a instalação do PWA');
       } else {
         console.log('Usuário recusou a instalação do PWA');
       }
-
-      // Limpar o evento de instalação adiado
-      deferredPrompt = null;
     });
-  }
-});
+  });
+  
